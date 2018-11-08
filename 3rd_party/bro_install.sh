3a40def843a5b02446a6cdf2fc94b6d533431c48
deadlift1226 <danemorgan91@gmail.com>
2018-11-08 06:41:34 -0700

    added pi stuff

--- 3rd_party/bro_install.sh
+++ 3rd_party/bro_install.sh
@@ -1,20 +1,19 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 #run install_pf_ring() first
-git clone https://github.com/ntop/PF_RING.git /root/.
-cd /root/PF_RING/kernel
-make && insmod ./pf_ring.ko
-cd ../userland && make
+#pf_ring_depens=' flex bison ethtool'
+#git clone https://github.com/ntop/PF_RING.git /root/PF_RING
+#cd /root/PF_RING/kernel
+#make && insmod ./pf_ring.ko
+#cd ../userland && make && make install
 
 bro_depens="git cmake make gcc g++ flex bison libpcap-dev libssl1.0-dev python-dev swig zlib1g-dev mailutils "
-echo "Installing needed packages:\n"+$bro_depens
-apt update -y && apt upgrade -y && apt install -y +$bro_depens
-git clone --recursive git://git.bro.org/bro /root/.
-cd bro
+echo "Installing needed packages:\n"$bro_depens
+apt update -y && apt upgrade -y && apt install -y $bro_depens
+#git clone --recursive git://git.bro.org/bro /root/bro
+cd /root/bro
 ./configure --with-pcap=/usr/local/lib && make && make install
-ln /usr/local/bro/bin/* /usr/bin
-
-echo "REMEMBER TO MAKE SURE INTERFACE IS CORRECT AT"
-echo "/usr/local/bro/etc/node.cfg"
-echo "REMEMBER TO MAKE SURE NETWORKS ARE CORRECT AT"
-echo "/usr/local/bro/etc/network.cfg"
+#echo "REMEMBER TO MAKE SURE INTERFACE IS CORRECT AT"
+#echo "/usr/local/bro/etc/node.cfg"
+#echo "REMEMBER TO MAKE SURE NETWORKS ARE CORRECT AT"
+#echo "/usr/local/bro/etc/network.cfg"
--- 3rd_party/katoolin_install.sh
+++ 3rd_party/katoolin_install.sh
@@ -1,4 +1,4 @@
 # must run as root
-git clone https://github.com/LionSec/katoolin.git
-cp katoolin/katoolin.py /usr/bin/katoolin
+git clone https://github.com/LionSec/katoolin.git /root/katoolin
+cp /root/katoolin/katoolin.py /usr/bin/katoolin
 chmod +x /usr/bin/katoolin
