568973dbc18b4986629bc91f59979df2f9c1da96
root <root@f3nr1r>
2018-12-03 13:19:58 -0700

    added all free maxmind mmdb

--- 3rd_party/maxmind_geolite.sh
+++ 3rd_party/maxmind_geolite.sh
@@ -1,1 +1,6 @@
-wget -N http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
+echo "Downloading GeoLite2-City"
+wget http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz
+echo "Downloading GeoLite2-Ccountry"
+wget http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.tar.gz
+echo "Downloading GeoLite2-ASN"
+wget http://geolite.maxmind.com/download/geoip/database/GeoLite2-ASN.tar.gz
