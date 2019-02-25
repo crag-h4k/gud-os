1768942ec068f59eec3496a7daeea7bb63567b1d
root <root@asgard>
2019-02-25 14:42:10 -0700

    vim install script done

---
+++ config/vim/install-vim.sh
@@ -0,0 +1,11 @@
+dst="/etc/vim"
+atld=$dst/autoload
+bundle=$dst/bundle
+
+mkdir -p $bundle $atld
+git init $dst
+git submodule add git://github.com/mephux/bro.vim.git $bundle/bro
+git submodule add git://github.com/luochen1990/rainbow $bundle/rainbow
+git submodule add git://github.com/flazz/vim-colorschemes $bundle/vim-colorschemes
+git submodule add git://github.com/ervandew/supertab $bundle/supertab
+git submodule add git://github.com/queezythegreat/vim-YouCompleteMe $bundle/YouCompleteMe
