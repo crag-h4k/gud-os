ab2cbd94c33c823322681d5fcefccba593f3383d
Dane Morgan <32076156+deadlift1226@users.noreply.github.com>
2019-03-26 05:24:52 -0600

    Update pi_pkgs.sh

--- pi/pi_pkgs.sh
+++ pi/pi_pkgs.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/env bash
-pi_pkgs=" build-essential byobu cmake curl dtrx git htop make gcc g++ libffi-dev libpython3-all-dev libssl-dev mailtools ncdu net-tools netselect-apt nmap python3-pip python3-jedi ranger sudo tree tmux vim wget zsh nmap wireshark "
+#!/usr/bin/env sh
+pi_pkgs=" build-essential byobu cmake curl dtrx git htop make gcc g++ libffi-dev libpython3-all-dev cmake make gcc g++ flex bison libmaxminddb-dev libmaxminddb0 libpcap-dev libssl1.0-dev python-dev swig zlib1g-dev mailutils mailtools ncdu net-tools netselect-apt nmap python3-pip python3-jedi ranger sudo tree tmux vim-nox vim-pathogen wget zsh nmap wireshark "
 
 apt update -y
 apt full-upgrade -y
