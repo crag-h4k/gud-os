150d40f092ed314791b5ec7a288d79a5cd74a7e9
root <root@f3nr1r.frig_off.net>
2019-03-28 17:18:14 -0600

    add elixir vim

--- config/vim/install-vim.sh
+++ config/vim/install-vim.sh
@@ -9,3 +9,4 @@ git submodule add git://github.com/luochen1990/rainbow $bundle/rainbow
 git submodule add git://github.com/flazz/vim-colorschemes $bundle/vim-colorschemes
 git submodule add git://github.com/ervandew/supertab $bundle/supertab
 git submodule add git://github.com/queezythegreat/vim-YouCompleteMe $bundle/YouCompleteMe
+git submodule add https://github.com/elixir-lang/vim-elixir.git bundle/vim-elixir
