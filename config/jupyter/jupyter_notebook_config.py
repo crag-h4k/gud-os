8a005b6fb853ec95cb67bc23a2cd9003632dba68
Dane Morgan <32076156+deadlift1226@users.noreply.github.com>
2019-04-19 05:23:18 -0600

    Add files via upload

---
+++ config/jupyter/jupyter_notebook_config.py
@@ -0,0 +1,13 @@
+# Configuration file for jupyter-notebook.
+c.NotebookApp.certfile = u'/home/'
+c.NotebookApp.keyfile = u'/home/'
+c.NotebookApp.open_browser = False
+
+c.NotebookApp.password = u'sha1:'
+c.NotebookApp.allow_remote_access = True
+c.NotebookApp.ip = '*'
+c.NotebookApp.port = 1234
+c.NotebookApp.base_url = u'/'
+c.NotebookApp.default_url = '/lab'
+c.NotebookApp.notebook_dir = u'/home/'
+#------------------------------------------------------------------------------
