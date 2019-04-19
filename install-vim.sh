5cd1ecf5a4b27383e1dbd8a4474f42a4f183ffb5
Dane Morgan <32076156+deadlift1226@users.noreply.github.com>
2019-04-19 05:26:02 -0600

    Delete install-vim.sh

--- install-vim.sh
+++
@@ -1,7 +0,0 @@
-git submodule add git://github.com/mephux/bro.vim.git $1/bro
-git submodule add https://github.com/davidhalter/jedi-vim $1/jedi-vim
-git submodule add git://github.com/luochen1990/rainbow $1/rainbow
-git submodule add git://github.com/ervandew/supertab $1/supertab
-git submodule add git://github.com/flazz/vim-colorschemes $1/vim-colorschemes
-git submodule add https://github.com/elixir-lang/vim-elixir.git $1/vim-elixir
-git submodule add https://github.com/wookayin/vim-typora $1/vim-typora
