946bede73446fb8e3bc34e55c3af34256a413ffb
Dane Morgan <damorgan@Danes-MacBook-Pro.local>
2019-05-25 08:10:10 -0600

    symlinks added to submodules

--- linux/config/vim/install-vim.sh
+++ linux/config/vim/install-vim.sh
@@ -1,14 +1,12 @@
 dst="~/.vim"
-atld=$dst/autoload
-bundle=$dst/bundle
+#atld=$dst/autoload
+bundle=./bundle
 
-mkdir -p $bundle $atld
-cd $dst
-git init $dst
+#mkdir -p $bundle $atld
+#cd $dst
+#git init $dst
 #rsync -a * $dst/.
-#git submodule add git://github.com/mephux/bro.vim.git $bundle/bro
-#git submodule add git://github.com/luochen1990/rainbow $bundle/rainbow
-#git submodule add git://github.com/flazz/vim-colorschemes $bundle/vim-colorschemes
-#git submodule add git://github.com/ervandew/supertab $bundle/supertab
-#git submodule add git://github.com/queezythegreat/vim-YouCompleteMe $bundle/YouCompleteMe
-#git submodule add https://github.com/elixir-lang/vim-elixir.git bundle/vim-elixir
+git submodule add git://github.com/mephux/bro.vim.git $bundle/bro
+git submodule add git://github.com/luochen1990/rainbow $bundle/rainbow
+git submodule add git://github.com/flazz/vim-colorschemes $bundle/vim-colorschemes
+git submodule add git://github.com/ervandew/supertab $bundle/supertab
